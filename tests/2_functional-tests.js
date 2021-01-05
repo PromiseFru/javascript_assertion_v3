35f22f5bdea1ea943158d7ef3046723febcd2b5d
Promise Fru <promisefru1@gmail.com>
2021-01-06 00:07:51 +0100

    updates made

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -87,11 +87,13 @@ suite("Functional Tests with Zombie.js", function () {
     });
     // #6
     test('submit "surname" : "Vespucci" - write your e2e test...', function (done) {
-      browser.assert.success();
-      browser.assert.text("span#name", "Amerigo");
-      browser.assert.text("span#surname", "Vespucci");
-      browser.assert.element("span#dates", 1)
-      done();
+      browser.fill("surname", "Colombo").pressButton("submit", function () {
+        browser.assert.success();
+        browser.assert.text("span#name", "Amerigo");
+        browser.assert.text("span#surname", "Vespucci");
+        browser.assert.element("span#dates", 1)
+        done();
+      });
     });
   });
 });
